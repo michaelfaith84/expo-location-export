7d34afa7a73671893b9ef9533a76150f48b349fb
mike <mpf84@pm.me>
2024-05-27 19:40:29 -0700

    Dependency pruning.

--- tests/gpx.test.ts
+++ tests/gpx.test.ts
@@ -1,71 +1,67 @@
-import GPX, { gpxNS, gpxSchema, gpxVersion, gpxXSI } from "../src/GPX";
+import GPX, {gpxNS, gpxSchema, gpxVersion, gpxXSI} from "../src/GPX";
 import PACKAGE_INFO from "../package.json";
-import {
-  defaultGlobal,
-  exampleGPSReturn,
-  exampleGPSReturnSeries,
-} from "./test.data";
-import { getBBox } from "../src/utilities";
-import { XMLParser, XMLValidator } from "fast-xml-parser";
+import {defaultGlobal, exampleGPSReturn, exampleGPSReturnSeries,} from "./test.data";
+import {getBBox} from "../src/utilities";
+import {XMLParser, XMLValidator} from "fast-xml-parser";
 import cloneDeep from "lodash.clonedeep";
 
 const parser = new XMLParser({
-  ignoreAttributes: false,
-  attributeNamePrefix: "attr-",
+    ignoreAttributes: false,
+    attributeNamePrefix: "attr-",
 });
 
 const gpx = new GPX();
 
 describe("GPX Point conversion", () => {
-  test("Single Point to Waypoint", () => {
-    const xml = gpx.toPoint(
-      [cloneDeep(exampleGPSReturn)],
-      {
-        ...defaultGlobal,
-        metadata: {
-          copyright: { license: "https://mit-license.org/" },
-        },
-      },
-      {},
-    );
-    expect(XMLValidator.validate(xml)).toBeTruthy();
-    const parsed = parser.parse(xml);
-    expect(parsed.gpx.metadata).toBeDefined();
-    expect(parsed.gpx.metadata.copyright.license).toEqual(
-      "https://mit-license.org/",
-    );
-    expect(parsed.gpx.metadata.copyright.year).toEqual(
-      new Date().getFullYear(),
-    );
-    expect(parseFloat(parsed.gpx.wpt["attr-lat"])).toEqual(
-      exampleGPSReturn.coords.latitude,
-    );
-    expect(parseFloat(parsed.gpx.wpt["attr-lon"])).toEqual(
-      exampleGPSReturn.coords.longitude,
-    );
-    expect(parseFloat(parsed.gpx["attr-version"])).toEqual(gpxVersion);
-    expect(parsed.gpx["attr-xmlns"]).toEqual(gpxNS);
-    expect(parsed.gpx["attr-xmlns:xsi"]).toEqual(gpxXSI);
-    expect(parsed.gpx["attr-xsi:schemaLocation"]).toEqual(gpxSchema);
-    expect(parsed.gpx["attr-creator"]).toEqual(
-      `${PACKAGE_INFO.name}@${PACKAGE_INFO.version}`,
-    );
-    expect(parsed.gpx.wpt.ele).toEqual(exampleGPSReturn.coords.altitude);
-  });
-
-  test("Many Points to Waypoint with Bounds", () => {
-    const xml = gpx.toPoint(cloneDeep(exampleGPSReturnSeries), defaultGlobal, {
-      bbox: getBBox(exampleGPSReturnSeries),
+    test("Single Point to Waypoint", () => {
+        const xml = gpx.toPoint(
+            [cloneDeep(exampleGPSReturn)],
+            {
+                ...defaultGlobal,
+                metadata: {
+                    copyright: {license: "https://mit-license.org/"},
+                },
+            },
+            {},
+        );
+        expect(XMLValidator.validate(xml)).toBeTruthy();
+        const parsed = parser.parse(xml);
+        expect(parsed.gpx.metadata).toBeDefined();
+        expect(parsed.gpx.metadata.copyright.license).toEqual(
+            "https://mit-license.org/",
+        );
+        expect(parsed.gpx.metadata.copyright.year).toEqual(
+            new Date().getFullYear(),
+        );
+        expect(parseFloat(parsed.gpx.wpt["attr-lat"])).toEqual(
+            exampleGPSReturn.coords.latitude,
+        );
+        expect(parseFloat(parsed.gpx.wpt["attr-lon"])).toEqual(
+            exampleGPSReturn.coords.longitude,
+        );
+        expect(parseFloat(parsed.gpx["attr-version"])).toEqual(gpxVersion);
+        expect(parsed.gpx["attr-xmlns"]).toEqual(gpxNS);
+        expect(parsed.gpx["attr-xmlns:xsi"]).toEqual(gpxXSI);
+        expect(parsed.gpx["attr-xsi:schemaLocation"]).toEqual(gpxSchema);
+        expect(parsed.gpx["attr-creator"]).toEqual(
+            `${PACKAGE_INFO.name}@${PACKAGE_INFO.version}`,
+        );
+        expect(parsed.gpx.wpt.ele).toEqual(exampleGPSReturn.coords.altitude);
     });
-    expect(XMLValidator.validate(xml)).toBeTruthy();
-  });
+
+    test("Many Points to Waypoint with Bounds", () => {
+        const xml = gpx.toPoint(cloneDeep(exampleGPSReturnSeries), defaultGlobal, {
+            bbox: getBBox(exampleGPSReturnSeries),
+        });
+        expect(XMLValidator.validate(xml)).toBeTruthy();
+    });
 });
 
 describe("GPX Track conversion", () => {
-  test("To Track with Bounds", () => {
-    const xml = gpx.toTrack(cloneDeep(exampleGPSReturnSeries), defaultGlobal, {
-      bbox: getBBox(exampleGPSReturnSeries),
+    test("To Track with Bounds", () => {
+        const xml = gpx.toTrack(cloneDeep(exampleGPSReturnSeries), defaultGlobal, {
+            bbox: getBBox(exampleGPSReturnSeries),
+        });
+        expect(XMLValidator.validate(xml)).toBeTruthy();
     });
-    expect(XMLValidator.validate(xml)).toBeTruthy();
-  });
 });
--- tests/kml.test.ts
+++ tests/kml.test.ts
@@ -1,56 +1,56 @@
-import { XMLParser } from "fast-xml-parser";
+import {XMLParser} from "fast-xml-parser";
 import KML from "../src/KML";
 import cloneDeep from "lodash.clonedeep";
-import {
-  defaultGlobal,
-  exampleGPSReturn,
-  exampleGPSReturnSeries,
-} from "./test.data";
+import {defaultGlobal, exampleGPSReturn, exampleGPSReturnSeries,} from "./test.data";
 
 const parser = new XMLParser({
-  ignoreAttributes: false,
-  attributeNamePrefix: "attr-",
+    ignoreAttributes: false,
+    attributeNamePrefix: "attr-",
 });
 
 const kml = new KML();
 
-describe("KML Point conversion", () => {
-  test("Single Point", () => {
-    const xml = kml.toPoint(
-      [cloneDeep(exampleGPSReturn)],
-      {
-        ...defaultGlobal,
-        id: "some point",
-      },
-      {},
-    );
-    expect(xml).toBeDefined();
-  });
+// TODO: Real tests for KML
 
-  test("Many Points", () => {
-    const xml = kml.toPoint(
-      cloneDeep(exampleGPSReturnSeries),
-      {
-        ...defaultGlobal,
-        metadata: {
-          copyright: { license: "https://mit-license.org/" },
-        },
-      },
-      {},
-    );
-    expect(xml).toBeDefined();
-  });
+describe("KML Point conversion", () => {
+    test("Single Point", () => {
+        const xml = kml.toPoint(
+            [cloneDeep(exampleGPSReturn)],
+            {
+                ...defaultGlobal,
+                id: "some point",
+            },
+            {},
+        );
+
+        expect(xml).toBeDefined();
+    });
+
+    test("Many Points", () => {
+        const xml = kml.toPoint(
+            cloneDeep(exampleGPSReturnSeries),
+            {
+                ...defaultGlobal,
+                metadata: {
+                    copyright: {license: "https://mit-license.org/"},
+                },
+            },
+            {},
+        );
+        expect(xml).toBeDefined();
+    });
 });
 
 describe("KML LineString conversion", () => {
-  test("Many Points", () => {
-    const xml = kml.toLineString(
-      cloneDeep(exampleGPSReturnSeries),
-      {
-        ...defaultGlobal,
-        id: "some line",
-      },
-      {},
-    );
-  });
+    test("Many Points", () => {
+        const xml = kml.toLineString(
+            cloneDeep(exampleGPSReturnSeries),
+            {
+                ...defaultGlobal,
+                id: "some line",
+            },
+            {},
+        );
+        expect(xml).toBeDefined();
+    });
 });
